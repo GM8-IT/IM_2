d67f4ab89a87ba3577c8a4ae4c9abcb1fdf95d0f
GM8-IT <gianmalasan8@gmail.com>
2025-09-09 15:58:10 +0800

    Add files via upload

---
+++ HW1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             while (true)
+        {
+            Console.Write("\nEnter string: ");
+            string input = Console.ReadLine();
+
+            if (input == "*")
+                break;
+
+            string original = input;
+            string lowerInput = input.ToLower();
+            string reversed = "";
+
+            for (int i = lowerInput.Length - 1; i >= 0; i--)
+            {
+                reversed += lowerInput[i];
+            }
+
+            string result = (lowerInput == reversed) ? "Yes" : "No";
+
+            Console.WriteLine("\nOriginal String         Reversed String            Palindrome?");
+            Console.WriteLine("---------------------------------------------------------------");
+            Console.WriteLine(original.PadRight(25) + reversed.PadRight(25) + result);
+        }
+
+        Console.WriteLine("\n\n//End execution");
+        }
+    }
+}
