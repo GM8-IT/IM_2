711926c7a1059ce431954a5553a71bb4c00a4fd2
GM8-IT <gianmalasan8@gmail.com>
2025-09-15 20:46:45 +0800

    Add files via upload

---
+++ SW1/SW1.2_ConvCurrency/SW1.2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter currency in ($): ");
+            String input = Console.ReadLine();
+            String[] s1 = input.Split(',');
+
+            Console.WriteLine();
+            Console.WriteLine("Dollar($)       Phil Peso(P)     Jpn Yen (Y)");
+            Console.WriteLine("--------------------------------------------");
+
+            foreach (String s2 in s1)
+            {
+                if(int.TryParse(s2, out int num)) {
+                    decimal peso = DtoP(num);
+                    decimal yen = DtoY(num);
+
+                    Console.WriteLine($"${num}          P{Math.Round(peso,2)}          ¥{Math.Floor(yen)}");
+                }else{
+                    Console.WriteLine($"Invalid input: '{num}'");
+                }
+            }
+            Console.ReadKey();
+        }
+        static decimal DtoP(decimal num)
+        {
+            return num * 57.16m;
+        }
+        static decimal DtoY(decimal num)
+        {
+            return num * 147.67m;
+        }
+    }
+}
---
+++ SW1/SW1_Prime.Compo/SW1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            Console.Write("Enter String: ");
+            String input = Console.ReadLine();
+            String[] sp1 = input.Split(' ');
+
+            foreach (String sp2 in sp1)
+            {
+                if (int.TryParse(sp2, out int number) && number > 1)
+                {
+                    counter++;
+                    if (Checker(number))
+                    {
+                        Console.WriteLine($"{counter}. {number} Prime");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{counter}. {number} Composite");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input.");
+                }
+            }
+            Console.ReadKey();
+        }
+        static bool Checker(int num)
+        {
+            if (num <= 1) return false;
+                for(int i=2; i <= Math.Sqrt(num); i++)
+                {
+                    if(num % i == 0)
+                    {
+                        return false;
+                    }
+                }
+            return true;
+        }
+    }
+}
