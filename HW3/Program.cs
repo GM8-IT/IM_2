60c4d20381c35f2020c45454d8a067f1b22e46c1
GM8-IT <gianmalasan8@gmail.com>
2025-10-03 22:56:35 +0800

    Add files via upload

---
+++ HW3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3
+{
+    class Program
+    {
+        static int[] GetCF(string input)
+        {
+            int[] freq = new int[256];
+
+            foreach (char c in input)
+            {
+                if (!char.IsWhiteSpace(c))
+                {
+                    freq[c]++;
+                }
+            }
+
+            return freq;
+        }
+
+        static void Main()
+        {
+            Console.Write("Enter string: ");
+            string input = Console.ReadLine();
+
+            int[] result = GetCF(input);
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result[i] > 0)
+                {
+                    Console.WriteLine($"{(char)i} = {result[i]}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+
+
+}
